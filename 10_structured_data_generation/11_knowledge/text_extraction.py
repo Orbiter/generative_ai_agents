10b5586beaaefac1021b8a85425589f47cfe4666
Michael Peter Christen <mc@yacy.net>
2024-10-14 23:51:00 +0200

    added list generation

--- 10_structured_data_generation/11_knowledge/text_extraction.py
+++ 10_structured_data_generation/11_knowledge/text_extraction.py
@@ -17,19 +17,31 @@ from chat_context import ChatContext, default_system_prompt
 with open(f"{archetypes_dir}/archetypes.json", "r") as json_data:
     archetypes = json.load(json_data)
 
-print(archetypes)
-
 # create a chat client
 chat_client = ChatClient(config_file=default_config_file)
 chat_context = ChatContext(system_prompt=default_system_prompt)
 group = "FAST"
-template = archetypes["knowledge"]["template"]
-prompt = archetypes["knowledge"]["prompt"].replace("<template>", template)
+
+lists_template = archetypes["lists"]["template"]
+lists_prompt = archetypes["lists"]["prompt"].replace("<template>", lists_template)
+question = "Name 10 cities in France"
+
+try:
+    assistant_reply = chat_client.chat(group = group, chat_context = chat_context, prompt=question + "\n" + lists_prompt)
+    
+    print(assistant_reply)
+except Exception as e:
+    print(f"Error: {e}", file=sys.stderr)
+    sys.exit(1)
+
+
+knowledge_template = archetypes["knowledge"]["template"]
+knowledge_prompt = archetypes["knowledge"]["prompt"].replace("<template>", knowledge_template)
 question = "Write an travelers description of France and it's capital, Paris."
 
 # Get the assistant's reply
 try:
-    assistant_reply = chat_client.chat(group = group, chat_context = chat_context, prompt=question + "\n" + prompt)
+    assistant_reply = chat_client.chat(group = group, chat_context = chat_context, prompt=question + "\n" + knowledge_prompt)
     
     print(assistant_reply)
 except Exception as e:
